096e2e4541963a19ce47300771def72e76184e81
Joãooo <mm2000_@hotmail.com>
2022-09-23 11:43:23 +0100

    add: collection page added

---
+++ app/src/main/java/com/example/musicmanager/RecordCollection.java
@@ -0,0 +1,14 @@
+package com.example.musicmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecordCollection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record_collection);
+    }
+}
